98be70971b79294a3c0a02837e64a8132ab1cdfc
rifkifauzans <rifki.fauzan3456@gmail.com>
2024-06-09 11:14:22 +0700

    Memperbaiki detail Order

--- resources/views/customer/detailOrder.blade.php
+++ resources/views/customer/detailOrder.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Page Header Start -->
-<br> <br> <br>
+<br><br><br>
 <div class="page-header container-fluid bg-secondary pt-2 pt-lg-5 pb-2 mb-5">
     <div class="container py-5">
         <div class="row align-items-center py-4">
@@ -11,9 +11,9 @@
             </div>
             <div class="col-md-6 text-center text-md-right">
                 <div class="d-inline-flex align-items-center">
-                    <a class="btn text-white" href="">Home</a>
-                    <i class="fas fa-angle-right text-white"></i>
-                    <a class="btn text-white disabled" href="">Detail Orders</a>
+                    <a class="btn text-white" href="#">Home</a>
+                    <i class="fas fa-angle-right text-white mx-2"></i>
+                    <a class="btn text-white disabled" href="#">Detail Orders</a>
                 </div>
             </div>
         </div>
@@ -24,63 +24,61 @@
 <div class="content-wrapper">
     <div class="container mt-5">
         <div class="card">
-            <div class="card-header">
-                <h2>Detail Order</h2>
+            <div class="card-header d-flex flex-column flex-md-row justify-content-between align-items-center">
+                <h2 class="mb-2 mb-md-0">Detail Order</h2>
                 <h3 class="text-right"><small>No Order:</small> {{ $order->order_number }}</h3>
             </div>
             <div class="card-body">
-                <h4>Customer</h4>
-                <table class="table table-bordered">
-                    <tr>
-                        <th>Nama</th>
-                        <td>{{ $order->customer->customer_name }}</td>
-                    </tr>
-                    <tr>
-                        <th>Nomor Telepon</th>
-                        <td>{{ $order->customer->phone_number }}</td>
-                    </tr>
-                    <tr>
-                        <th>Alamat</th>
-                        <td>{{ $order->customer->address }}</td>
-                    </tr>
-                    <tr>
-                        <th>Order Masuk</th>
-                        <td>{{ \Carbon\Carbon::parse($order->order_date)->format('d-m-Y') }}</td>
-                    </tr>
-                    <tr>
-                        <th>Diambil Pada</th>
-                        <td>{{ $order->delivery_date ? \Carbon\Carbon::parse($order->delivery_date)->format('d-m-Y') : 'Not set' }}</td>
-                    </tr>
-                    <tr>
-                        <th>Durasi Kerja</th>
-                        <td>{{ $order->duration }} Hari</td>
-                    </tr>
-                    <tr>
-                        <th>Jenis Paket</th>
-                        <td>{{ $order->category->type_laundry }}</td>
-                    </tr>
-                </table>
-
-                <h4>Order</h4>
-                <table class="table table-bordered">
-                    <tr>
-                        <th>Berat (Kg)</th>
-                        <th>Harga Per-Kg</th>
-                        <th>Total Bayar</th>
-                    </tr>
-                    <tr>
-                        <td>{{ $order->quantity_kg }} Kg</td>
-                        <td>Rp. {{ number_format($order->category->price, 0, ',', '.'),00 }}</td>
-                        <td>Rp. {{ number_format($order->total_price, 0, ',', '.'),00 }}</td>
-                    </tr>
-                </table>
-
-                <div class="details">
-                    <h4 class="mb-3">Keterangan:</h4>
-                    <p>{{ $order->description }}</p>
+                <div class="mb-4">
+                    <h4>Customer</h4>
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>Nama</th>
+                                <td>{{ $order->customer->customer_name }}</td>
+                            </tr>
+                            <tr>
+                                <th>Nomor Telepon</th>
+                                <td>{{ $order->customer->phone_number }}</td>
+                            </tr>
+                            <tr>
+                                <th>Alamat</th>
+                                <td>{{ $order->customer->address }}</td>
+                            </tr>
+                            <tr>
+                                <th>Order Masuk</th>
+                                <td>{{ \Carbon\Carbon::parse($order->order_date)->format('d-m-Y') }}</td>
+                            </tr>
+                            <tr>
+                                <th>Diambil Pada</th>
+                                <td>{{ $order->delivery_date ? \Carbon\Carbon::parse($order->delivery_date)->format('d-m-Y') : 'Not set' }}</td>
+                            </tr>
+                            <tr>
+                                <th>Jenis Paket</th>
+                                <td>{{ $order->category->type_laundry }}</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
 
-                <div class="form-footer">
+                <div class="mb-4">
+                    <h4>Order</h4>
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>Berat (Kg)</th>
+                                <th>Harga Per-Kg</th>
+                                <th>Total Bayar</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $order->quantity_kg }} Kg</td>
+                                <td>Rp {{ number_format($order->category->price, 0, ',', '.') }},00</td>
+                                <td>Rp {{ number_format($order->total_price, 0, ',', '.') }},00</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="form-footer text-right">
                     <a href="#" class="btn btn-primary">Bayar Sekarang</a>
                 </div>
             </div>
