aa72b2b24b6428bd4d96ff66a3deb38e2d3adbfe
Anggi <angginilamsari3420@gmail.com>
2024-06-15 15:01:45 +0700

    Menambahkan route Orders pd halaman admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -33,7 +33,7 @@
               <div class="icon">
                 <i class="fas fa-shopping-cart" style="color: white"></i>
               </div>
-              <a href="" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ route('listOrders') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
